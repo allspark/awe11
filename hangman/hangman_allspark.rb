ef718f8cec3db92a3d0be4486d5164d498442262
Dennis Boerm <allspark@planetcyborg.de>
2011-01-21 13:31:12 +0100

    try with 3 lines

--- hangman/hangman_allspark.rb
+++ hangman/hangman_allspark.rb
@@ -1,6 +1,3 @@
 #!/usr/bin/ruby
-c, word, count = [], proc {|s| s[rand(s.size)] }.call(["letmein", "hello world"]), proc { |w,c| w.split(//).uniq.join.count(c.join) }
-begin
-  print(word.gsub(/[^#{c.join.empty? ? "." : c.join}\s]/, '_'),"  (", c.size - count.call(word, c), " bad tries)\n")
-  c=(c+((gets or break).strip[0] or next).split(//))
-end while (c.size - count.call(word, c) < 10 && word.gsub(/[^#{c.join.empty? ? "." : c.join}\s]/, '_') != word)
+c, word, count, actWord = [], proc {|s| s[rand(s.size)] }.call(["letmein", "hello world"]), proc { |w,c| c.size - w.split(//).uniq.join.count(c.join) }, proc { |w,c| w.gsub(/[^#{c.join.empty? ? "." : c.join}\s]/, '_') }
+print(actWord.call(word, c),"  (", count.call(word, c), " bad tries)\n", (actWord.call(word, c) == word ? "geschafft" : "")) while (count.call(word, c) < 10 && actWord.call(word, (c=(c+((gets or break).strip[0] or next).split(//)))) != word)
