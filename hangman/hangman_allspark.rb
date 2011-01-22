94cd39469558e34411fdd61202f5f7537997ff2b
Dennis Boerm <allspark@planetcyborg.de>
2011-01-22 15:34:38 +0100

    4 lines :)

--- hangman/hangman_allspark.rb
+++ hangman/hangman_allspark.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/ruby
-c, word, count, actWord = [], proc {|s| s[rand(s.size)] }.call(["letmein", "hello world"]), proc { |w,c| c.size - w.split(//).uniq.join.count(c.join) }, proc { |w,c| w.gsub(/[^#{c.join.empty? ? "." : c.join}\s]/, '_') }
-print(actWord.call(word, c),"  (", count.call(word, c), " bad tries)\n", (actWord.call(word, c) == word ? "geschafft" : "")) while (count.call(word, c) < 10 && actWord.call(word, (c=(c+((gets or break).strip[0] or next).split(//)))) != word)
+i, w, c, aW, f = [], proc {|s| s[rand(s.size)] }.call(["letmein", "hello world"]), proc { |w,c| c.size - w.split(//).uniq.join.count(c.join) }, proc { |w,c| w.gsub(/[^#{c.join.empty? ? "." : c.join}\s]/, '_') }, true
+begin
+  print(aW.call(w, i),"  (", c.call(w, i), " bad tries)\n", ((f=!(aW.call(w, i) == w)) ? "" : "geschafft\n"))  
+end while (c.call(w, i) < 10 && f && (i=(i+(((t=gets) or break).strip[0] != nil ? t.split(//) : []))))
