891146ce739a87f59e1c6fc611a2175c982f87be
Dennis Boerm <allspark@planetcyborg.de>
2011-02-01 14:36:47 +0100

    code cleaning

--- hangman/hangman_allspark.rb
+++ hangman/hangman_allspark.rb
@@ -2,4 +2,4 @@
 i, w, c, aW, f = [], proc {|s| s[rand(s.size)] }.call(["letmein", "hello world"]), proc { |w,c| c.size - w.split(//).uniq.join.count(c.join) }, proc { |w,c| w.gsub(/[^#{c.join.empty? ? "." : c.join}\s]/, '_') }, true
 begin
   print(aW.call(w, i),"  (", c.call(w, i), " bad tries)\n", ((f=(aW.call(w, i) != w)) ? "" : "geschafft\n"))  
-end while (c.call(w, i) < 10 && f && (i=(i+((t=((gets) or break).strip[0]) != nil ? t.split(//) : []))))
+end while (c.call(w, i) < 10 && f && (i=(i+((t=(gets or break).strip[0]) != nil ? t.split(//) : []))))
