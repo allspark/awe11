4ea2cddf8f0ec0e896a9dc66dae11872b18cf0c7
msquare <msquare@planetcyborg.de>
2011-02-02 09:35:28 +0100

    handman output upgrade

--- hangman/hangman_msquare.rb
+++ hangman/hangman_msquare.rb
@@ -1,6 +1,6 @@
 input, missed, word = "", 0, ["foobar", "schrumpfschniepel", "saftsack", "wir lieben awe"][rand(4)]
 begin
-	puts word.gsub(/[^ #{input}]{1}/i, "_") + " (" + (input.size==0 || word.include?(input[input.size-1]) ? missed.to_s + " Fehler)" : (missed += 1).to_s + " Fehler)\nBisherige Eingabe: " + input)
+	puts word.gsub(/[^ #{input}]{1}/i, "_") + " (" + input.size.to_s + "/" + (word.size * 2).to_s + ", " + (input.size==0 || word.include?(input[input.size-1]) ? missed.to_s + " Fehler)" : (missed += 1).to_s + " Fehler)\nBisherige Eingabe: " + input)
 	(missed == 10) ? (puts "Zu viele Fehler!") : (input += (gets or return)[0])
 end while (missed < 10 && word.gsub(/[^ #{input}]/i, "_") != word && (input.size < 2 * word.size))
 (word.gsub(/[^ #{input}]{1}/i, "_") == word) ? (puts "Geschafft:\n" + word) : (puts "Nicht geschafft:\n" + word)
