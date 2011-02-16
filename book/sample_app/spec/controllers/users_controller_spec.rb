4c6a6080dde6db9c249b76459c4393e72df441f0
Dennis Boerm <allspark@planetcyborg.de>
2011-02-17 00:33:55 +0100

    factory, test

--- book/sample_app/spec/controllers/users_controller_spec.rb
+++ book/sample_app/spec/controllers/users_controller_spec.rb
@@ -11,7 +11,24 @@ describe UsersController do
 
     it "should have the right title" do
       get 'new'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Sign up")
+      response.should have_selector("title", :content => "Sign up")
+    end
+  end
+  
+  describe "GET 'show'" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
     end
   end
 end
