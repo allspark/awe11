c370c3b10c0e63177246c7f469241fae02dcba81
Dennis Boerm <allspark@planetcyborg.de>
2011-02-19 18:39:14 +0100

    11.3.3

--- book/sample_app/app/controllers/microposts_controller.rb
+++ book/sample_app/app/controllers/microposts_controller.rb
@@ -2,11 +2,12 @@ class MicropostsController < ApplicationController
   before_filter :authenticate, :only => [:create, :destroy]
 
   def create
-    @micropost  = current_user.microposts.build(params[:micropost])
+    @micropost = current_user.microposts.build(params[:micropost])
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_path
     else
+      @feed_items = []
       render 'pages/home'
     end
   end
